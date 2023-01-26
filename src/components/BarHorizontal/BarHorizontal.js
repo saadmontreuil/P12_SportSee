2795e6e8c834e09c101fc2edfd595011b18d8976
saadmontreuil <79247132+saadmontreuil@users.noreply.github.com>
2023-01-26 17:57:12 +0100

    Horizontal & Vertical bar components

---
+++ src/components/BarHorizontal/BarHorizontal.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../images/logo.svg'
+import styles from './BarHorizontal.module.css'
+
+export default function BarHorizontal() {
+  return (
+    <nav className={styles.horizontalBar}>
+      <Link to="/" className="logoBar">
+        <img src={logo} alt="SportSee" className={styles.logo} />
+      </Link>
+
+      <ul className={styles.navMenu}>
+        <li className="item">
+          <Link to="/" className={styles.navLink}>
+            Accueil
+          </Link>
+        </li>
+        <li className="item">
+          <Link to="/profil" className={styles.navLink}>
+            Profil
+          </Link>
+        </li>
+        <li className="item">
+          <Link to="/reglage" className={styles.navLink}>
+            Réglage
+          </Link>
+        </li>
+        <li className="item">
+          <Link to="/communaute" className={styles.navLink}>
+            Communauté
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
---
+++ src/components/BarVertical/BarVertical.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './BarVertical.module.css'
+import { Icon } from '@iconify/react';
+
+
+export default function BarVertical() {
+  return (
+    <div className={styles.verticalBar}>
+      <div className={styles.navIcon}>
+        <div className={styles.icon}>
+            <Icon icon="mdi:meditation" color="red" />
+        </div>
+        <div className={styles.icon}>
+            <Icon icon="grommet-icons:swim" color="red" />
+        </div>
+        <div className={styles.icon}>
+            <Icon icon="material-symbols:directions-bike-rounded" color="red" />
+        </div>
+        <div className={styles.icon}>
+            <Icon icon="mdi:weights" color="red" />
+        </div>
+
+      </div>
+      <p className={styles.navText}>Copyright, SportSee 2020</p>
+    </div>
+  )
+}
